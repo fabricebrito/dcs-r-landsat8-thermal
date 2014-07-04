3ceaed048e326593c486e1a48d82e4e790bc3117
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-04 20:35:04 +0200

    Update and rename CalcThermal.R to BrightnessTemperature.R

---
+++ src/main/R/rLandsat8/R/BrightnessTemperature.R
@@ -0,0 +1,26 @@
+#' creates a raster with the brightness temperature extrcted from Landsat tirs1 band
+#' @description Creates a raster with the brightness temperature extrcted from Landsat tirs1 band.
+#'
+#' @param product name of the product, e.g. LC80522102014165LGN00. It must be in the working directory.
+#' @return brightness temperature raster
+#' @examples \dontrun{
+#' BrightnessTemperature("LC80522102014165LGN00")
+#' }
+#'
+#' @export
+#' @import raster
+
+BrightnessTemperature <- function(product) {
+  
+  l <- ReadLandsat8(product)
+
+  ml <- as.numeric(l$metadata$radiance_mult_band_10)
+  al <- as.numeric(l$metadata$radiance_add_band_10)
+  k1 <- as.numeric(l$metadata$k1_constant_band_10)
+  k2 <- as.numeric(l$metadata$k2_constant_band_10)
+
+  bt <- k2 / log(k1 / (ml * l$band$tirs1 + al) + 1) - 273.15 
+
+  return(bt)
+
+}
