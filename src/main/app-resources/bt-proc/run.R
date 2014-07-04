c62ea9ea9ab550ff3b9beda96ca18079b5948d08
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-04 21:06:18 +0200

    Update run.R

--- src/main/app-resources/bt-proc/run.R
+++ src/main/app-resources/bt-proc/run.R
@@ -3,6 +3,8 @@
 # load rciop library to access the developer cloud sandbox functions
 library("rciop")
 
+library(stringr)
+
 # load the application package when mvn installed it
 library(rLandsat8, lib.loc="/application/share/R/library")
 
@@ -14,6 +16,17 @@ open(f)
 while(length(ls8.ref <- readLines(f, n=1)) > 0) {
   
   rciop.log("INFO", paste("processing product", ls8.ref))
+  
+  ls8.url <- rciop.casmeta(field="dclite4g:onlineResource", url=l8.ref)$output
+  
+  ls8.identifier <- strsplit(rciop.casmeta(field="dc:identifier", url=l8.ref)$output, ":")[[1]][2]
+  
+  DownloadLandsat(url=ls8.url, output.name=ls8.identifier)
+  
+  # extract the compressed Landsat 8 product
+  
+  # calculate the brightness temperature
+  bt <- BrightnessTemperature(ls8.identifier)
   
   
   
