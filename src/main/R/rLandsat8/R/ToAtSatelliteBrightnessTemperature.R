f22b78eb7382106517605ce8484fb5d7692dc8e7
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-06 16:00:57 +0200

    Update ToAtSatelliteBrightnessTemperature.R

--- src/main/R/rLandsat8/R/ToAtSatelliteBrightnessTemperature.R
+++ src/main/R/rLandsat8/R/ToAtSatelliteBrightnessTemperature.R
@@ -4,22 +4,31 @@
 #' @param product name of the product, e.g. LC80522102014165LGN00. It must be in the working directory.
 #' @return brightness temperature raster
 #' @examples \dontrun{
-#' ToAtSatelliteBrightnessTemperature("LC80522102014165LGN00")
+#' ls8 <- ReadLandsat8("LC80522102014165LGN00"
+#' ToAtSatelliteBrightnessTemperature(ls8, "tirs1)
 #' }
 #'
 #' @export
 #' @import raster
 
-ToAtSatelliteBrightnessTemperature <- function(product) {
+ToAtSatelliteBrightnessTemperature <- function(landsat8, band) {
   
-  l <- ReadLandsat8(product)
+  bandnames <-c("aerosol", "blue", "green", "red",
+  "nir", "swir1", "swir2",
+  "panchromatic",
+  "cirrus",
+  "tirs1", "tirs2")
+  
+  # todo stop if band != tirs1, tirs2
+  
+  toarad <- ToTOARadiance(landsat8, band)
+  
+  idx <- seq_along(bandnames)[sapply(bandnames, function(x) band %in% x)]
 
-  ml <- as.numeric(l$metadata$radiance_mult_band_10)
-  al <- as.numeric(l$metadata$radiance_add_band_10)
-  k1 <- as.numeric(l$metadata$k1_constant_band_10)
-  k2 <- as.numeric(l$metadata$k2_constant_band_10)
+  k1 <- as.numeric(landsat8$metadata$[[paste0("k1_constant_band_",idx)]]
+  k2 <- as.numeric(landsat8$metadata$[[paste0("k2_constant_band_",idx)]]
 
-  bt <- k2 / log(k1 / (ml * l$band$tirs1 + al) + 1) - 273.15 
+  bt <- k2 / log(k1 / toarad + 1) - 273.15 
 
   return(bt)
 
