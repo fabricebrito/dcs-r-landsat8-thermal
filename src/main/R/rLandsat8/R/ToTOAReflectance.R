b88e02a923226b4d91c6da2c46d6bd18d5d511dc
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-06 15:53:02 +0200

    Create ToTOAReflectance.R

---
+++ src/main/R/rLandsat8/R/ToTOAReflectance.R
@@ -0,0 +1,34 @@
+#' creates a raster with the TOA reflectance
+#' @description Creates a raster with the TOA reflectance
+#'
+#' @param landsat8 list returned by rLandsat8::ReadLandsat8
+#' @param band Landsat 8 bandname (one of "aerosol", "blue", "green", "red", "nir", "swir1", "swir2", "panchromatic", "cirrus", "tirs1", "tirs2"
+#' @return TOA Reflectance raster
+#' @examples \dontrun{
+#' ls8 <- ReadLandsat8("LC80522102014165LGN00"
+#' r <- ToTOAReflectance(ls8, "tirs1)
+#' }
+#'
+#' @export
+#' @import raster
+
+ToTOARadiance <- function(landsat8, band) {
+
+  bandnames <-c("aerosol", "blue", "green", "red",
+  "nir", "swir1", "swir2",
+  "panchromatic",
+  "cirrus",
+  "tirs1", "tirs2")
+  
+  # todo check if band is in bandnames
+
+  idx <- seq_along(bandnames)[sapply(bandnames, function(x) band %in% x)]
+
+  ml <- landsat8$metadata$[[paste0("reflectance_mult_band_",idx)]]
+  al <- landsat8$metadata$[[paste0("reflectance_add_band_",idx)]]
+  
+  TOAref <- landsat8$band[[band]] * ml + al
+  
+  return(TOAref)
+  
+}
