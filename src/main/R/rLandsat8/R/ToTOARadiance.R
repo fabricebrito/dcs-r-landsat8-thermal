ce5fc1e0121868351d1df9f409a253acd1d85a88
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-06 15:49:43 +0200

    Update ToTOARadiance.R

--- src/main/R/rLandsat8/R/ToTOARadiance.R
+++ src/main/R/rLandsat8/R/ToTOARadiance.R
@@ -1,8 +1,34 @@
+#' creates a raster with the TOA radiance 
+#' @description Creates a raster with the TOA radiance
+#'
+#' @param landsat8 list returned by rLandsat8::ReadLandsat8
+#' @param band Landsat 8 bandname (one of "aerosol", "blue", "green", "red", "nir", "swir1", "swir2", "panchromatic", "cirrus", "tirs1", "tirs2" 
+#' @return TOA Radiance raster
+#' @examples \dontrun{
+#' ls8 <- ReadLandsat8("LC80522102014165LGN00"
+#' r <- ToTOARadiance(ls8, "tirs1)
+#' }
+#'
+#' @export
+#' @import raster
 
+ToTOARadiance <- function(landsat8, band) {
 
-ToTOARadiance <- function(band) {
-
-  band.title <- 
+  bandnames <-c("aerosol", "blue", "green", "red",
+  "nir", "swir1", "swir2",
+  "panchromatic",
+  "cirrus",
+  "tirs1", "tirs2")
   
+  # todo check if band is in bandnames
 
+  idx <- seq_along(bandnames)[sapply(bandnames, function(x) band %in% x)]
+
+  ml <- landsat8$metadata$[[paste0("radiance_mult_band_",idx)]]
+  al <- landsat8$metadata$[[paste0("radiance_add_band_",idx)]]
+  
+  TOArad <- landsat8$band[["tirs1"]] * ml + al
+  
+  return(TOArad)
+  
 }
