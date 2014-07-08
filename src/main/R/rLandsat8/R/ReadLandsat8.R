1ad25fac279188a168abbaa2c114073529055f0b
Fabrice Brito <fabrice.brito@terradue.com>
2014-07-08 11:03:24 +0200

    Update ReadLandsat8.R

--- src/main/R/rLandsat8/R/ReadLandsat8.R
+++ src/main/R/rLandsat8/R/ReadLandsat8.R
@@ -45,6 +45,7 @@ ReadLandsat8 <- function(product) {
   bands=lapply(raster.files, function(x) {
     r <- raster(paste0(product, "/", met[[x]]))
     r@title <- names(raster.files)[seq_along(raster.files)[sapply(raster.files, function(a) x %in% a)]]
+    NAvalue(r) <- 0
     return(r)
   }) 
   
